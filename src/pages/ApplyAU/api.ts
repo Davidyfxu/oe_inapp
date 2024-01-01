29217cec6f2be552d3f8e312e023f97e9d8b61c8
davidyfxu <82054510@qq.com>
2024-01-01 12:16:34 +0800

    feat: transfer

--- src/pages/ApplyAU/api.ts
+++ src/pages/ApplyAU/api.ts
@@ -1,4 +1,4 @@
 import { post } from "./utils";
 
-export const get_au_case = (p: any = {}) => post("/AU/getCases", p);
-export const create_au_case = (p: any = {}) => post("/AU/create", p);
+export const get_au_case = (p: any = {}) => post("/au/getCases", p);
+export const create_au_case = (p: any = {}) => post("/au/createCase", p);
