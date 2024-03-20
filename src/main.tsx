5ffde84c3ed76055b2a335220f42db276c98625d
davidyfxu <82054510@qq.com>
2024-03-20 21:11:53 +0800

    feat: push

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ConfigProvider, theme } from "antd";
-
+const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
+  <ConfigProvider
+    theme={{
+      algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
+    }}
+  >
     <App />
   </ConfigProvider>,
 );
